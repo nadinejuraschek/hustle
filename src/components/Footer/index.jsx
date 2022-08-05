9f66f782ce8a12be58eef9e06e99f23365ebb4e1
nadinejuraschek <nadinejwebdev@gmail.com>
2022-08-05 11:21:45 +0200

    New: author name changed, node version specified

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,8 +4,8 @@ import styles from './footer.module.css';
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <span>© { new Date().getFullYear() }</span>
-      <a href='https://github.com/nadinejuraschek'>Nadine Juraschek</a>
+      <span>© {new Date().getFullYear()}</span>
+      <a href='https://github.com/nadinejuraschek'>Nadine Pesso</a>
     </footer>
   );
 };
