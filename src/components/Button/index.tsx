721d50cf0946443f595e9ab9e0e7e0dc3f3f971e
nadinejuraschek <nadinejwebdev@gmail.com>
2022-08-12 13:29:59 +0200

    New: typescript setup

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,6 +1,11 @@
+import { ButtonProps } from './types';
 import styles from './button.module.css';
 
-const Button = ({ disabled, label, outline }) => (
+const Button = ({
+  disabled = false,
+  label,
+  outline = false,
+}: ButtonProps): JSX.Element => (
   <button
     className={`${styles.btn} ${outline ? styles.outline : ''} ${
       disabled ? styles.disabled : ''
