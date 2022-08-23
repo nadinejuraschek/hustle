3e6e637337693c183a528fca965cce0c227fa622
nadinejuraschek <nadinejwebdev@gmail.com>
2022-08-23 21:44:27 +0200

    New: hooks in typescript

--- src/hooks/useWindowSize.tsx
+++ src/hooks/useWindowSize.tsx
@@ -1,15 +1,20 @@
 import { useEffect, useState } from 'react';
 
-export const useWindowSize = () => {
+export type Size = {
+  height: number | undefined;
+  width: number | undefined;
+};
+
+export const useWindowSize = (): Size => {
   // Initialize state with undefined width/height so server and client renders match
   // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
-  const [windowSize, setWindowSize] = useState({
+  const [windowSize, setWindowSize] = useState<Size>({
     width: undefined,
     height: undefined,
   });
 
   // Handler to call on window resize
-  const handleResize = () => {
+  const handleResize = (): void => {
     // Set window width/height to state
     setWindowSize({
       width: window.innerWidth,
